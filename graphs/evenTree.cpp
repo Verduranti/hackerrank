ed2395c77a6e62057c68c5ef2f720de25456b877
verduranti <angelofchaos@gmail.com>
2015-01-28 13:15:14 -0800

    Saving this because it got me 40/40 but I think it’s wrong.

--- graphs/evenTree.cpp
+++ graphs/evenTree.cpp
@@ -6,9 +6,8 @@
 using namespace std;
 
 
-//Note: I'm already 99.9% sure this code will only work for an ACYCLICAL graph
+//Note: I'm 100% sure this code will only work for an ACYCLICAL graph
 //Otherwise it'll get stuck. Honest. Don't try it.
-
 
 //AH I remember why we use pointers now. Damn implied copy contructors for everything.
 //Change the one I pointed you to, dang it!
@@ -20,6 +19,7 @@ class Node
 		int name;
 		vector<Node*> children;
 		int countChildren();
+		Node* parent;
 //I guess to be proper this thing should have accessors and shit. MEH.
 
 };
@@ -27,6 +27,7 @@ class Node
 Node::Node(int name)
 {
 	this->name = name;
+	parent = NULL;
 }
 
 int Node::countChildren()
@@ -37,8 +38,14 @@ int Node::countChildren()
 		vector<Node*>::iterator iter;
 		for(iter = this->children.begin(); iter < this->children.end(); iter++)
 		{
-			count += 1; // for the current child
-			count += (*iter)->countChildren(); // for the grandchildren
+			if((*iter)->parent != NULL) //Gloriously hacktastic
+			//This lovely hack will make sure that I only process children who
+			//want to continue to be children, not ones that have freed themselves
+			//by setting their parent to null
+			{
+				count += 1; // for the current child
+				count += (*iter)->countChildren(); // for the grandchildren
+			}
 		}
 	}
 	return count;
@@ -48,6 +55,7 @@ int main() {
 	int numNodes = 0, numEdges = 0;
     cin >> numNodes >> numEdges;
     vector<Node*> nodes;
+//     vector<pair<int, int>> edges;
     for(int i = 0; i < numNodes; i++)
     {
     	Node* newNode = new Node(i+1);
@@ -61,26 +69,40 @@ int main() {
     	{
     		Node* node = nodes.at(v-1);
     		Node* child = nodes.at(u-1);
+    		child->parent = nodes.at(v-1);
     		node->children.push_back(child);
     	}
     	else
     	{    	
     		Node* node = nodes.at(u-1);
     		Node* child = nodes.at(v-1);
+    		child->parent = nodes.at(u-1);
     		node->children.push_back(child);
     	}
     }
     
-    int countExtra = 0; //this actually counts the number of trees in the resulting forest...
+    int countTree = 0;
+    //Turns out you can reverse iterate through the tree from the leaf nodes back up
+    //and if you find an even tree, sever it and increment the counter (1 new tree, one
+    //removed edge). Viola, problem sloved.
+    
+    vector<Node*> newRoots;
     vector<Node*>::reverse_iterator rIter;
     for(rIter = nodes.rbegin(); rIter < nodes.rend(); rIter++)
     {
     	Node* node = *rIter;
     	if(node->countChildren()%2 > 0)
-    		countExtra++;
+    	{
+    		if(node->parent != NULL)
+    		{
+    		    countTree++;
+//     			Node* parent = node->parent;
+//     			node->parent = NULL;
+    		}
+    	}
     }
     
-    cout << countExtra;
+    cout << countTree;
     
     return 0;
 }
