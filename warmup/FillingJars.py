04648c7c3ef8f5861d9a5d7b25608bbc24a095dc
verduranti <angelofchaos@gmail.com>
2015-03-05 16:45:28 -0800

    Fixed!

--- warmup/FillingJars.py
+++ warmup/FillingJars.py
@@ -12,19 +12,3 @@ for i in range(numOps):
 	sum += candies * (index2 + 1 - index1)
 
 print(sum//numJars)
-
-#candyJars = zeros(numJars)
-
-#list_of_lists = []
-# for i in range(numOps):
-# 	list = input().split(" ")
-# 	list_of_lists.append(list)
-# 
-# for x in list_of_lists:
-# 	index1 = int(x[0])-1
-# 	index2 = int(x[1]) 
-# 	candies = int(x[2])
-# 	for y in range(index1, index2):
-# 		candyJars[y] += candies
-# 	
-# print(sum(candyJars)//len(candyJars))
